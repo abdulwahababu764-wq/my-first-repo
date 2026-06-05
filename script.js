3b4fdfda47fe393428a9c40db8cfd8cf127623b5
Abdulwahab <git config --global user.email>
2026-06-05 17:21:21 +0300

    comment line number 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  // Mobile menu toggle
   const hamburger = document.querySelector('.hamburger');
   const navMenu = document.querySelector('.nav-menu');
 
